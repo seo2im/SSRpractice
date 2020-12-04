83aa07f9b60ef9429b3798b46fb5d73790eb1440
soe2im <soe2im6492@gmail.com>
2020-12-04 14:16:12 +0900

    caching update

--- Advanced/srcs/server.js
+++ Advanced/srcs/server.js
@@ -1,27 +1,26 @@
+import React from 'react'
 import express from 'express'
 import fs from 'fs';
 import path from 'path'
-import { renderToString } from 'react-dom/server' 
-import React from 'react'
-import App from './App'
 import * as url from'url'
 
 import { renderPage, prerenderPages } from './common'
 
+import lruCache from 'lru-cache'
+const ssrCache = new lruCache({
+	max : 100,
+	maxAge : 1000 * 60,
+})
+
 const app = express();
-/*
-const html = fs.readFileSync(
-	path.resolve(__dirname, '../dist/index.html'),
-	'utf-8'
-)
-*/
+
 const prerenderHtml = {};
 for (const page of prerenderPages) {
 	const pageHtml = fs.readFileSync(
 		path.resolve(__dirname, `../dist/${page}.html`),
 		'utf8'
 	)
-	prerenderPages[page] = pageHtml;
+	prerenderHtml[page] = pageHtml;
 }
 
 app.use('/dist', express.static('dist'))
@@ -29,16 +28,24 @@ app.use('/dist', express.static('dist'))
 app.get('./favicon.ico', (req, res) => res.sendStatus(204));
 app.get('*', (req, res) => {
 	const parsedUrl = url.parse(req.url, true);
+	const cacheKey = parseUrl.path;
+	/* Get cache */
+	if (ssrCache.has(cacheKey)) {
+		res.send(ssrCache.get(cacheKey));
+		return ;
+	}
 	const page = parsedUrl.pathname ? parsedUrl.pathname.substr(1) : 'home'
 	const initialData = { page };
 
 	/* render only when no pre rendering */
-	const pageHtml = prerenderPages.includes(page)
-		? prerenderPages[page]
+	const pageHtml = prerenderHtml.includes(page)
+		? prerenderHtml[page]
 		: renderPage(page);
 	const result = pageHtml.replace(
 		'__DATA_FROM_SERVER__', JSON.stringify(initialData)
 	)
+	/* Save cache */
+	ssrCache.set(cacheKey, result);
 	res.send(result);
 })
 
